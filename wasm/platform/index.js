77a70f0a12f1d59d71e06db878e882ca2cba177b
BrightCraft <72756158+brightcraft@users.noreply.github.com>
2026-09-21 14:31:34 +0200

    Add duplicate check log and comment adjustment

--- wasm/platform/index.js
+++ wasm/platform/index.js
@@ -1713,7 +1713,10 @@ function formatUpdateTimestamp(ms) {
 // Show the Update App button when a new update is found
 function updateAppButton(latestVersion) {
   // Prevent adding duplicate buttons if one already exists
-  if ($('#updateAppBtn').length > 0) return;
+  if ($('#updateAppBtn').length > 0) {
+    console.log('%c[index.js, updateAppButton]', 'color: green;', 'Update App button already exists. Skipping duplicate creation!');
+    return;
+  }
 
   // Create the button dynamically
   var updateAppBtn = $('<button>', {
@@ -1867,7 +1870,7 @@ function checkForAppUpdates() {
       if (checkVersionUpdate(appInfo.version, latestVersion)) {
         // Show the Update Moonlight dialog with new version and release notes to inform user to update the app
         updateAppDialog(latestVersion, releaseNotes);
-        // Create and display the Update App button at the top of the app so they can access it later if they close the dialog
+        // Create and display the Update App button so they can access it later if they close the dialog
         updateAppButton(latestVersion);
       } else {
         // Otherwise, show a snackbar message to inform the user that the app is already up to date
